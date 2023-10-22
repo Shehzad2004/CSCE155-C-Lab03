0457bcb5ce98c121995780c0262243aa1e2d7734
shehzad2004 <fk166494@gmail.com>
2023-10-22 19:37:34 +0500

    tax.c is completed

--- taxes.c
+++ taxes.c
@@ -29,6 +29,36 @@ int main(int argc, char **argv) {
   }
 
   //TODO: compute the tax, child credit, and total tax here
+  if(agi>0 && agi<=19900){
+    tax=0.1*agi;}
+    else if(agi>19900 && agi<=81050){
+      tax=(0.12*agi)+1990;
+    }
+    else if(agi>81050 && agi<172750)
+    {
+      tax=(0.22*agi)+9328;
+    }
+    else if(agi>172751 && agi<=329850){
+      tax=((0.24*agi)+29502);
+
+    }
+    else if(agi>329850 && agi<=418850){
+      tax=((0.32*agi)+67206);
+    }
+    else if(agi>418850 && agi<=628300){
+      tax=((0.35*agi)+95686);
+    }
+    else if(agi>628300)
+      tax=((0.37*agi)+168933.50);
+      
+    if(numChildren>0){
+      childCredit=numChildren*2000;
+    }
+    totalTax=tax-childCredit;
+    if(totalTax<=0){
+      totalTax=0;
+    }
+
 
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
